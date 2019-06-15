2fcd824753e0e4936528c5a626695b81590ac050
gyubraj <51144248+gyubraj@users.noreply.github.com>
2019-06-15 18:29:06 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,107 @@
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Calendar c=Calendar.getInstance();
+		int choice;
+		int day=c.get(Calendar.DAY_OF_MONTH);
+		int hour=c.get(Calendar.HOUR);
+		int min=c.get(Calendar.MINUTE);
+		int sec=c.get(Calendar.SECOND);
+		int mse=c.get(Calendar.MILLISECOND);
+		if(day==15&&hour==00&&min==00&&sec==00&&mse==000) {
+			SendMail sm=new SendMail();
+			sm.send();
+			
+		}
+		Scanner sc=new Scanner(System.in);
+		do
+		{
+		System.out.println("\n\n\t--------------------------------------------------------------------------");
+		System.out.print("\n\tEnter 1.Bank\n\t2.Atm\n\t3.Exit\n");
+		System.out.print("\n\tEnter your choice:");
+		choice =sc.nextInt();
+		switch(choice) {
+		case 1:
+			do
+			{
+			System.out.println("\t--------------------------------------------------------------------------");
+			System.out.print("\n\tEnter 1.Create Account\n\t2.Deposite\n\t3.Transfer\n\t4.Make Atm Card\n\t5.Exit\n" );
+			System.out.print("\n\tEnter your choice:");
+			choice =sc.nextInt();
+			switch(choice){
+			case 1:
+				CreateAccount ca=new CreateAccount();
+				ca.createAccount();
+				break;
+			case 2:
+				Deposite de=new Deposite();
+				de.depositeAmount();
+				break;
+			case 3:
+				Transfer tr=new Transfer();
+				tr.transferMoney();
+				break;
+			case 4:
+				GetAtm ga=new GetAtm();
+				ga.makeAtm();
+				break;
+			case 5:
+				break;
+			default:
+				System.out.println("\n\tInput Correct Choice.");
+			}
+			}while(choice!=5);
+			break;
+		case 2:
+			AccessAtm aa=new AccessAtm();
+			int a=aa.accessAtm();
+			if(a==1) {
+				do
+				{
+				System.out.println("\t--------------------------------------------------------------------------");
+				System.out.println("\n\tEnter 1.Account Information\n\t2.WithDraw Money\n\t3.MiniStatement\n\t4.Change Pin\n\t5.Exit");
+				System.out.print("\n\tEnter your choice:");
+				choice =sc.nextInt();
+				switch(choice) {
+				case 1:
+					AccountInformation ai=new AccountInformation();
+					ai.accountInformation();
+					break;
+				case 2:
+					WithDraw wd=new WithDraw();
+					wd.withDrawMoney();
+					break;
+				case 3:
+					MiniStatement ms=new MiniStatement();
+					ms.miniStatement();
+					break;
+				case 4:
+					ChangePin cp=new ChangePin();
+					cp.changePin();
+					break;
+				case 5 :
+					break;
+				default:
+					System.out.println("\n\tInput right Choice.");
+				}
+				
+			}while(choice!=5);
+			}
+			break;
+		case 3:
+			System.out.println("Thank you for using Service");
+			System.exit(0);
+		default:
+			System.out.print("\n\tWrong Input.\n");
+			
+		}
+		}while(choice!=3);
+		
+		
+	
+	}
+}
